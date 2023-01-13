9c412b7d674783b74b417c1524c8011433da9c63
Grigori <demgrisha@gmail.com>
2023-01-13 19:06:05 +0300

    made Random class

--- include/Math.hpp
+++ include/Math.hpp
@@ -1,21 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-class Vector2f {
-public:
-	float x, y;
+namespace math {
 
-	Vector2f(): x(0.0f), y(0.0f) { }
+	class Vector2f {
+	public:
+		float x, y;
 
-	Vector2f(float p_x, float p_y): x(p_x), y(p_y) { }
+		Vector2f(): x(0.0f), y(0.0f) { }
 
-	~Vector2f() = default;
+		Vector2f(float p_x, float p_y): x(p_x), y(p_y) { }
 
-	void setX(float p_x);
-	void setY(float p_y);
+		~Vector2f() = default;
 
-	void print();
-};
+		void setX(float p_x);
+		void setY(float p_y);
+
+		void print();
+	};
+
+	class Random {
+	public:
+		// Returns random int
+		static int randInt(int p_min, int p_max);
+	};
+}
--- src/Math.cpp
+++ src/Math.cpp
@@ -4,7 +4,15 @@
 
 using namespace std;
 
-void Vector2f::setX(float p_x) { x = p_x; }
-void Vector2f::setY(float p_y) { y = p_y; }
+namespace math {
 
-void Vector2f::print() { cout << x << ", " << y << endl; }
+	void Vector2f::setX(float p_x) { x = p_x; }
+	void Vector2f::setY(float p_y) { y = p_y; }
+
+	void Vector2f::print() { cout << x << ", " << y << endl; }
+
+
+	int Random::randInt(int p_min, int p_max) {
+		return min + (rand() % static_cast<int>(max - min + 1));
+	}
+}
